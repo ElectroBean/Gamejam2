43c283c9c6fec16ce31dc9d6efd5eccbd22d21dc
Deeon Burgan <dburgan@live.com>
2018-04-16 12:28:17 +1000

    Added platforms and their movement

---
+++ Assets/Scripts/PlatformMovement/PlatformMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+    
+    public float scrollSpeed;
+    public EndlessRunner er;
+    bool hasSpawnedNext;
+
+    private void Awake()
+    {
+        er = GameObject.FindGameObjectWithTag("GameManager").GetComponent<EndlessRunner>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+        //rb.MovePosition(transform.position + -transform.right * moveSpeedRB);
+        Scroll();
+
+        
+	}
+
+    void Scroll()
+    {
+        transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(!hasSpawnedNext)
+            {
+                er.SpawnPrefab(transform.position);
+                hasSpawnedNext = true;
+            }
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
+
