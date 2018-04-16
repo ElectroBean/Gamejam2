76aac0383fb7c37801e93cbb6f13aadd12c12e4b
Deeon Burgan <dburgan@live.com>
2018-04-16 16:23:59 +1000

    Added menu button functions

--- Assets/Scripts/Managers/Sceneloader.cs
+++ Assets/Scripts/Managers/Sceneloader.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Sceneloader : MonoBehaviour {
 
@@ -16,11 +17,16 @@ public class Sceneloader : MonoBehaviour {
 
     public void ToMenu()
     {
-
+        SceneManager.LoadScene(0);
     }
 
     public void ToGame()
     {
+        SceneManager.LoadScene(1);
+    }
 
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
