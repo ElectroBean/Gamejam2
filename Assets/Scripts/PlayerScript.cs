047b6ac70054ca5c4e6b823422130e3d3ba1f477
AIE\s171011 <gabrielpilakis@me.com>
2018-04-16 10:50:37 +1000

    Added MovementScene

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    public float jumpheight;
+    public bool Ground = false; 
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(transform.right * speed);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpheight);
+        }
+
+       
+        //rb.velocity = new Vector3(speed, 0, 0);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
