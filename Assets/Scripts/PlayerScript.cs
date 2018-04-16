05bd8278c20ddf5d90323b66ae2882401e9f919d
Gabriel <gabrielpilakis@me.com>
2018-04-16 16:18:10 +1000

    Added Jump and Attack buttons

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,32 +8,46 @@ public class PlayerScript : MonoBehaviour {
     public float speed;
     public float jumpheight;
     protected Collider coll; 
-    public bool Ground = false;
-    Ray myRay; 
+    public bool Canjump = false;
+    public bool CanSlide = false;
+    public bool CanAttack = false; 
     // Use this for initialization
     void Start () {
         coll = GetComponent<Collider>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        // if (Physics.Raycast(transform.position,downPosition))
-
-       if ( Physics.Raycast(transform.position, Vector3.down, coll.bounds.extents.y))
-            {
-              if (Input.GetKey(KeyCode.Space))
-                {
-                rb.AddForce(transform.up * jumpheight);
-            }
-        }
-     
-       
-	}
-
-  
-    public void ButtonClick()
+    // Update is called once per frame
+    void Update()
     {
-       // Debug.Log("test");
+        // if (Physics.Raycast(transform.position,downPosition))
+        if (Canjump == true)
+            if (Physics.Raycast(transform.position, Vector3.down, coll.bounds.extents.y))
+            {
+
+                rb.AddForce(transform.up * jumpheight * 10);
+                Canjump = false;
+            }
+    }
+       
+  public void Jump()
+    {
+        Update();
+        Canjump = true; 
+    }
+
+    public void Attack()
+    {
+        //OnTriggerEnter(coll);
+        CanAttack = true; 
+    }
+    private void OnTriggerStay(Collider coll)
+    {
+        if (CanAttack == true && coll.tag == "Enemy")
+        {
+           Destroy ( GameObject.FindWithTag("Enemy"));
+           Debug.Log("Enemy HIt"); 
+           CanAttack = false;
+        }
+        CanAttack = false;
     }
 }
